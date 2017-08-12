35071e4f8b3996b22113a66bf9343103283ee043
RatioChris <cjohnsen@weareratio.com>
2017-08-11 17:27:26 -0700

    so many updates

--- src/components/Sequencer/index.js
+++ src/components/Sequencer/index.js
@@ -1,69 +1,82 @@
 import React, { Component } from 'react'
 import Tone from 'tone'
-// import styles from './styles.css'
-// import Instruments from '../../utils/Instruments'
+import _ from 'underscore'
+import './styles.css'
+import Instruments from '../../utils/Instruments'
 
-let sequencer = null
+let sequencer
+let measures = 8
+let beatsPerMeasure = 4
+let instruments = new Instruments(measures, beatsPerMeasure)
+let bass = null
+let guitar = null
 let synth = null
 
 class Sequencer extends Component {
   constructor (props) {
     super(props)
 
-    this.arr = []
-    this.arrFixed = []
+    // this.arrMatrix = [{'row': 0, 'column': 0}, {'row': 3, 'column': 2}, {'row': 5, 'column': 4}, {'row': 0, 'column': 7}, {'row': 0, 'column': 16}, {'row': 3, 'column': 18}, {'row': 5, 'column': 20}, {'row': 3, 'column': 9}, {'row': 6, 'column': 11}, {'row': 5, 'column': 12}, {'row': 3, 'column': 23}, {'row': 0, 'column': 25}]
+    this.arrMatrix = [{'row': 0, 'column': 0}, {'row': 3, 'column': 2}, {'row': 5, 'column': 4}, {'row': 0, 'column': 7}, {'row': 0, 'column': 16}, {'row': 3, 'column': 18}, {'row': 5, 'column': 20}, {'row': 3, 'column': 9}, {'row': 6, 'column': 11}, {'row': 5, 'column': 12}, {'row': 3, 'column': 23}, {'row': 0, 'column': 25}, {'row': 7, 'column': 0}, {'row': 10, 'column': 2}, {'row': 12, 'column': 4}, {'row': 7, 'column': 7}, {'row': 10, 'column': 9}, {'row': 13, 'column': 11}, {'row': 12, 'column': 12}, {'row': 10, 'column': 18}, {'row': 12, 'column': 20}, {'row': 7, 'column': 16}, {'row': 10, 'column': 23}, {'row': 7, 'column': 25}, {'row': 0, 'column': 2}, {'row': 0, 'column': 4}, {'row': 0, 'column': 6}, {'row': 0, 'column': 18}, {'row': 0, 'column': 20}, {'row': 0, 'column': 8}, {'row': 0, 'column': 10}, {'row': 0, 'column': 12}, {'row': 0, 'column': 14}, {'row': 0, 'column': 30}, {'row': 0, 'column': 28}, {'row': 0, 'column': 26}, {'row': 0, 'column': 24}, {'row': 0, 'column': 22}]
+    this.arrGuitar = [{'row': 0, 'column': 0}, {'row': 3, 'column': 2}, {'row': 5, 'column': 4}, {'row': 0, 'column': 7}, {'row': 0, 'column': 16}, {'row': 3, 'column': 18}, {'row': 5, 'column': 20}, {'row': 3, 'column': 9}, {'row': 6, 'column': 11}, {'row': 5, 'column': 12}, {'row': 3, 'column': 23}, {'row': 0, 'column': 25}, {'row': 7, 'column': 0}, {'row': 10, 'column': 2}, {'row': 12, 'column': 4}, {'row': 7, 'column': 7}, {'row': 10, 'column': 9}, {'row': 13, 'column': 11}, {'row': 12, 'column': 12}, {'row': 10, 'column': 18}, {'row': 12, 'column': 20}, {'row': 7, 'column': 16}, {'row': 10, 'column': 23}, {'row': 7, 'column': 25}]
+    this.arrBass = [{'row': 0, 'column': 0}, {'row': 0, 'column': 16}, {'row': 0, 'column': 2}, {'row': 0, 'column': 4}, {'row': 0, 'column': 6}, {'row': 0, 'column': 18}, {'row': 0, 'column': 20}, {'row': 0, 'column': 8}, {'row': 0, 'column': 10}, {'row': 0, 'column': 12}, {'row': 0, 'column': 14}, {'row': 0, 'column': 30}, {'row': 0, 'column': 28}, {'row': 0, 'column': 26}, {'row': 0, 'column': 24}, {'row': 0, 'column': 22}]
+    this.arrNotes = []
+    this.matrix = []
   }
 
   componentDidMount () {
     this.initTone()
     this.initNexus()
-    this.initSynth()
+    this.setNotesFromMatrix()
+    bass = instruments.getBass()
+    guitar = instruments.getGuitar()
+    synth = instruments.getGuitar()
   }
 
-  throttle (type, name, obj) {
-    obj = obj || window
-    var running = false
-    var func = function () {
-      if (running) { return }
-      running = true
+  throttle (type, name) {
+    let isPaused = false
+    const func = function () {
+      if (isPaused) { return }
+      isPaused = true
       requestAnimationFrame(function () {
-        obj.dispatchEvent(new CustomEvent(name))
-        running = false
+        window.dispatchEvent(new CustomEvent(name))
+        isPaused = false
       })
     }
-    obj.addEventListener(type, func)
+    window.addEventListener(type, func)
   }
 
   initTone () {
-    Tone.Transport.bpm.value = 120
-    Tone.Transport.loopEnd = '2m'
+    Tone.Transport.bpm.value = 240
+    Tone.Transport.loopEnd = `${measures}m`
     Tone.Transport.loop = true
-    Tone.Transport.start(1)
+    Tone.Transport.start(3)
 
-    const part = new Tone.Part(function (time, value) {
-    // the value is an object which contains both the note and the velocity
-      synth.triggerAttackRelease(value.note, '8n', time)
-    }, this.arrFixed)
-    part.start(0)
+    /* const part = new Tone.Part(function (time, value) {
+      synth.triggerAttackRelease(value.note, `${measures * beatsPerMeasure}n`, time)
+    }, this.arrNotes)
+    part.start(0) */
 
     Tone.Transport.scheduleRepeat((time) => {
-      console.log('initTone 2m')
-      this.arrFixed.forEach((i) => {
-        console.log(i)
-
+      console.log(`initTone ${measures}m`, time)
+      this.arrNotes.forEach((i) => {
+        // console.log(i)
         Tone.Transport.scheduleOnce(() => {
-          synth.triggerAttackRelease(i.note, '8n')
+          synth.triggerAttackRelease(i.note, `${measures * beatsPerMeasure}n`)
         }, i.time)
       })
-    }, '2m')
+    }, `${measures}m`)
   }
 
   initNexus () {
     sequencer = new window.Nexus.Sequencer('#sequencer', {
-      'size': [window.innerWidth, 200],
+      'size': [window.innerWidth, 300],
       'mode': 'toggle',
-      'rows': 4,
-      'columns': 8
+      'rows': 14,
+      'columns': measures * beatsPerMeasure
+    })
+    this.arrMatrix.forEach((i) => {
+      sequencer.matrix.set.cell(i.column, i.row, 1)
     })
 
     sequencer.on('change', (v) => {
@@ -75,114 +88,62 @@ class Sequencer extends Component {
         // console.log('Draw', time)
         sequencer.next()
       }, time)
-    }, '4n')
+    }, `${beatsPerMeasure}n`)
 
     this.throttle('resize', 'optimizedResize')
     window.addEventListener('optimizedResize', () => {
-      sequencer.resize(window.innerWidth, 200)
+      sequencer.resize(window.innerWidth, 300)
+      // console.warn(JSON.stringify(sequencer.matrix.pattern))
+      // console.warn(JSON.stringify(this.matrix))
+      // sequencer.matrix.set.all(this.matrix)
+
+      this.arrMatrix.forEach((i) => {
+        sequencer.matrix.set.cell(i.column, i.row, 1)
+      })
     })
   }
 
-  initSynth () {
-    synth = new Tone.PolySynth(6, Tone.MonoSynth).toMaster()
-    synth.set({
-      'oscillator': {
-        'type': 'square'
-      },
-      'envelope': {
-        'attack': 0.1
+  setNotesFromMatrix () {
+    this.arrMatrix.forEach((i) => {
+      let beat = {
+        time: instruments.getTime(i.column),
+        note: instruments.getNote(i.row)
       }
+      this.arrNotes.push(beat)
     })
   }
 
   updateSynth (v) {
+    console.warn('updateSynth')
     console.log(v)
-    const col = v.column
-    const row = v.row
     const state = v.state
     delete v.state
-    let time
-    let note
+    this.matrix = sequencer.matrix.pattern
 
-    // let i = `${v.col}.${v.row}`
-    console.warn(state, v)
+    let beat = {
+      time: instruments.getTime(v.column),
+      note: instruments.getNote(v.row)
+    }
+
     if (state) {
-      this.arr.push(v)
+      if (_.find(this.arrMatrix, v) === undefined) {
+        this.arrMatrix.push(v)
+        this.arrNotes.push(beat)
+      }
     } else {
-      let arr = this.arr
-      console.warn('before', this.arr)
-      this.arr = arr.filter((i) => {
+      let arrMatrix = this.arrMatrix
+      let arrNotes = this.arrNotes
+      this.arrMatrix = arrMatrix.filter((i) => {
         // console.warn(i, v, JSON.stringify(i) !== JSON.stringify(v))
         return JSON.stringify(i) !== JSON.stringify(v)
       })
-      console.warn('after', this.arr)
-    }
-    console.log('arr', this.arr, JSON.stringify(this.arr))
-
-    switch (col) {
-      case 0:
-        time = '0:0'
-        break
-      case 1:
-        time = '0:1'
-        break
-      case 2:
-        time = '0:2'
-        break
-      case 3:
-        time = '0:3'
-        break
-      case 4:
-        time = '1:0'
-        break
-      case 5:
-        time = '1:1'
-        break
-      case 6:
-        time = '1:2'
-        break
-      case 7:
-        time = '1:3'
-        break
-      default:
-        break
-    }
-
-    switch (row) {
-      case 0:
-        note = 'F#3'
-        break
-      case 1:
-        note = 'E3'
-        break
-      case 2:
-        note = 'D3'
-        break
-      case 3:
-        note = 'C3'
-        break
-      default:
-        break
-    }
-
-    let beat = {
-      time: time,
-      note: note
-    }
-
-    if (state) {
-      this.arrFixed.push(beat)
-    } else {
-      let arr = this.arrFixed
-      console.warn('before', this.arrFixed)
-      this.arrFixed = arr.filter((i) => {
+      this.arrNotes = arrNotes.filter((i) => {
         // console.warn(i, v, JSON.stringify(i) !== JSON.stringify(beat))
         return JSON.stringify(i) !== JSON.stringify(beat)
       })
-      console.warn('after', this.arrFixed)
     }
-
-    console.log('arrFixed', this.arrFixed, JSON.stringify(this.arrFixed))
+    console.log('arrMatrix', this.arrMatrix, JSON.stringify(this.arrMatrix))
+    console.log('arrNotes', this.arrNotes, JSON.stringify(this.arrNotes))
 
     /* if (state) {
       Tone.Transport.scheduleOnce(() => {
@@ -196,11 +157,11 @@ class Sequencer extends Component {
 
     /* let part = new Tone.Part((time, note) => {
       // synth.triggerAttackRelease(note, '8n', time)
-      console.log(time, note, this.arrFixed[0])
+      console.log(time, note, this.arrNotes[0])
       Tone.Transport.schedule(() => {
         synth.triggerAttackRelease(note, '8n')
       }, time)
-    }, [this.arrFixed])
+    }, [this.arrNotes])
     part.start(0) */
   }
 
--- src/utils/Instruments.js
+++ src/utils/Instruments.js
@@ -1,11 +1,179 @@
 import Tone from 'tone'
 
+let beats = []
+let notes = [
+  'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2', 'C3', 'C#3', 'D3', 'D#3',
+  'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3', 'C4', 'C#4', 'D4', 'D#4',
+  'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5', 'D#5'
+]
+
+export default class Instruments {
+  constructor (measures, beatsPerMeasure) {
+    beats = this.setBeats(measures, beatsPerMeasure)
+  }
+
+  setBeats (measures, beatsPerMeasure) {
+    return _setBeats(measures, beatsPerMeasure)
+  }
+  setNotes (qty) {
+    return _setNotes(qty)
+  }
+
+  getNote (index) {
+    return notes[index]
+  }
+  getTime (index) {
+    return beats[index]
+  }
+
+  /**
+   * Instruments
+   */
+  getBass () {
+    return bass()
+  }
+  getGuitar () {
+    return guitar()
+  }
+  getSynth () {
+    return synth()
+  }
+}
+
+const _setNotes = (qty) => {
+  let arr = []
+  for (var i = 0; i < qty; i++) {
+    arr.push(notes[i])
+  }
+
+  return arr
+}
+
+const _setBeats = (measures, beatsPerMeasure) => {
+  let bar = 0
+  let beat = 0
+  for (var i = 0; i < (measures * beatsPerMeasure); i++) {
+    beats.push(`${bar}:${beat}`)
+    beat++
+    if (beat % beatsPerMeasure === 0) {
+      beat = 0
+      bar++
+    }
+  }
+
+  return beats
+}
+
 /**
- * Low Pass Filter
+ * Effects
  */
+const autoFilter = new Tone.AutoFilter({
+  frequency: '1',
+  wet: 1,
+  depth: 0.75
+})
+
+const autoWah = new Tone.AutoWah({
+  baseFrequency: 120,
+  octaves: 1,
+  Q: 1,
+  gain: 30
+})
+
+const chorus = new Tone.Chorus()
+
+const delay = new Tone.PingPongDelay({
+  delayTime: 0.25,
+  feedback: 0.25,
+  wet: 0.1
+})
+
+const distortion = new Tone.Distortion({
+  distortion: 1,
+  oversample: '4x',
+  wet: 1
+})
+
+const phaser = new Tone.Phaser({
+  frequency: 0.5,
+  octaves: 3,
+  baseFrequency: 3000
+})
+
+const pitch = new Tone.PitchShift({
+  pitch: 0,
+  windowSize: 0.5,
+  wet: 1
+})
+
+const reverb = new Tone.Freeverb({
+  roomSize: 0.8,
+  dampening: 6000
+})
+
+const portamento = new Tone.Monophonic({
+  portamento: 0.25
+})
+
 const lowPass = new Tone.Filter({
-  'frequency': 14000
+  frequency: 14000
 }).toMaster()
+
+/**
+ * Synth
+ */
+const synth = () => {
+  let inst = new Tone.PolySynth(12, Tone.MonoSynth).chain(delay, reverb, lowPass)
+  inst.set({
+    oscillator: {
+      type: 'square'
+    },
+    envelope: {
+      attack: 0.1,
+      release: 2
+    }
+  })
+
+  return inst
+}
+
+/**
+ * Guitar
+ */
+const guitar = () => {
+  let inst = new Tone.PolySynth(12, Tone.MonoSynth).chain(delay, distortion, reverb, lowPass)
+  inst.set({
+    oscillator: {
+      type: 'square'
+    },
+    envelope: {
+      attack: 0.1,
+      release: 2
+    }
+  })
+
+  return inst
+}
+
+/**
+ * Bass
+ */
+const bass = () => {
+  let inst = new Tone.PolySynth(12, Tone.MonoSynth).chain(autoWah, lowPass)
+  inst.set({
+    oscillator: {
+      type: 'square'
+    },
+    envelope: {
+      attack: 0.001,
+      decay: 0.1,
+      sustain: 0.9,
+      release: 2
+    }
+  })
+
+  return inst
+}
 
 /**
  * Hi-hat Open
@@ -26,12 +194,7 @@ const openHiHat = new Tone.NoiseSynth({
     'octaves': -2.5,
     'exponent': 4
   }
-}).connect(lowPass)
-
-const openHiHatPart = new Tone.Part(function (time) {
-  openHiHat.triggerAttack(time)
-}, ['2*8n', '6*8n'])
-// openHiHatPart.start(0)
+})
 
 /**
  * Hi-hat Closed
@@ -52,9 +215,4 @@ const closedHiHat = new Tone.NoiseSynth({
     'octaves': -2.5,
     'exponent': 4
   }
-}).connect(lowPass)
-
-const closedHatPart = new Tone.Part(function (time) {
-  closedHiHat.triggerAttack(time)
-}, ['0*8n', '1*8n', '3*8n', '4*8n', '5*8n', '7*8n', '8*8n'])
-// closedHatPart.start(0)
+})
